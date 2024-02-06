4a96d0fe8fa3366bb7a4464a78c621a920dbfd7c
Ali Tim <ali.tim03.at@gmail.com>
2024-02-06 08:15:17 +0600

    create reg pages

--- FileServer/main/form.py
+++ FileServer/main/form.py
@@ -6,3 +6,11 @@ class CreateUserFrom(UserCreationForm):
     class Meta:
         model = User
         fields = ['email', 'password1', 'password2']
+        exclude = ['username', ]
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.username = user.email
+        if commit:
+            user.save()
+        return user
--- FileServer/main/views.py
+++ FileServer/main/views.py
@@ -1,21 +1,23 @@
-from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
 
 from .form import CreateUserFrom
 
 
-def index(req):
-    return render(req, 'main/index.html')
+def base(req):
+    return render(req, 'main/base.html')
 
 
 def reg(req):
     form = CreateUserFrom()
+    # form.username = "req.POST['email']"
 
     if req.method == 'POST':
+        print(req.POST['email'])
+        # form.username = 'jjljlbj'
         form = CreateUserFrom(req.POST)
 
         if form.is_valid():
+            # form.initial.username = req.POST['email']
             form.save()
 
     ctx = {'form': form}
